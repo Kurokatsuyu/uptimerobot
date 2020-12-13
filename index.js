b7bace0754eddea04c8778efabb8cd5cf641dd5d
Kurokatsuyu <75931067+Kurokatsuyu@users.noreply.github.com>
2020-12-13 17:54:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://indovibes-sto.glitch.me"]
+const urls = ["hhttps://indovibes-store.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
